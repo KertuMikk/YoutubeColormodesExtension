3807d6f72e0075b0efae4f5af06d4cc2d9acdf47
Kertu Mikk <kertumik@tlu.ee>
2018-04-18 13:44:45 +0300

    youtube lehel popup avaneb

--- background.js
+++ background.js
@@ -11,7 +11,7 @@ chrome.runtime.onInstalled.addListener(function () {
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'https://www.youtube.com/*'}
+        pageUrl: { hostEquals: 'www.youtube.com', schemes: ['https'] }
       })
       ],
       actions: [new chrome.declarativeContent.ShowPageAction()]
