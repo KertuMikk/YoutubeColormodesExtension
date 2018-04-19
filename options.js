c3970c1d88ff23697f03f2d9d077939852d0f75f
Kärt Nigols <kart.nigols@gmail.com>
2018-04-19 21:36:47 +0300

    Gray theme

--- options.js
+++ options.js
@@ -1,7 +1,7 @@
 
 'use strict'
 
-const kButtonTheme = ['rainbow', 'pastel', 'red', 'navy']
+const kButtonTheme = ['rainbow', 'pastel', 'red', 'navy', 'B&W']
 
 function constructOptions (kButtonTheme) {
   for (let item of kButtonTheme) {
