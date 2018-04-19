6ef15abc86e10a9f4eaa0d5ad9c6285ead4bd5ae
Kertu Mikk <kertumik@tlu.ee>
2018-04-19 10:11:41 +0300

    colormode pics

--- options.js
+++ options.js
@@ -6,7 +6,9 @@ const kButtonColors = ['rainbow', 'pastel', 'red']
 function constructOptions (kButtonColors) {
   for (let item of kButtonColors) {
     let button = document.createElement('button')
-    button.style.backgroundImage = "url('C:/users/ketuo/eesrakendused/3.ea-kodutoo/images/" + item + ".png')"
+    button.style.height = 100 + 'px'
+    button.style.width = 100 + 'px'
+    button.style.backgroundImage = "url('https://raw.githubusercontent.com/KertuMikk/3.ea-kodutoo/master/images/" + item + ".png')"
     button.addEventListener('click', function () {
       chrome.storage.sync.set({color: item + '.css'}, function () {
         console.log('color is ' + item)
