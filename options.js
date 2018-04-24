4efadee1ff2f86bd39cfa1183f774d7b4275ca2b
Kertu Mikk <kertumik@tlu.ee>
2018-04-24 18:25:45 +0300

    color names in options page

--- options.js
+++ options.js
@@ -9,12 +9,18 @@ function constructOptions (kButtonTheme) {
     button.style.height = 40 + 'px'
     button.style.width = 40 + 'px'
     button.style.backgroundImage = "url('https://raw.githubusercontent.com/KertuMikk/YoutubeColormodesExtension/master/images/" + item + ".png')"
-    button.addEventListener('click', function () {
+    let name = document.createElement('p') /* COMMENT OUT FROM LET NAME TO ELEMENT.APPEND.. TO MOVE COLOR SELECTION TO OPTIONS  */
+    let node = document.createTextNode(item)
+    name.appendChild(node)
+    let element = document.getElementById('buttonDiv')
+    element.appendChild(name)
+    /* button.addEventListener('click', function () {
       chrome.storage.sync.set({color: "url('https://raw.githubusercontent.com/KertuMikk/YoutubeColormodesExtension/master/images/" + item + ".png')", item: item}, function () {
         console.log('color is ' + item)
-      })
-    })
+      })                             UNCOMMENT TO MOVE COLOR SELECTION TO OPTIONS */
+    /* } /*) */
     document.getElementById('buttonDiv').appendChild(button)
-  }
+  } /* COMMENT OUT TO MOVE COLOR SELECTION TO OPTIONS */
 }
+/* } */
 constructOptions(kButtonTheme)
