609c1267c5155d2dd67d62743a609c70d3814f51
Kertu Mikk <kertumik@tlu.ee>
2018-05-12 16:37:12 +0300

    finally working

--- loadCss.js
+++ loadCss.js
@@ -3,21 +3,22 @@
 /* var a = document.createElement('script')
   a.scr = 'test.js'
   a.type = 'text/javascript'
-  document.getElementById('head').appendChild(a)
-  chrome.storage.sync.get('item', function (data) {
-    console.log('color is ' + data.item)
-    this.window.insertCSS({file: 'css/' + data.item})
-  }) */
-let item
+  */
+
 chrome.storage.sync.get('item', function (data) {
-  item = data.item
+  let link = document.createElement('link')
+  link.href = chrome.extension.getURL('css/' + data.item)
+  link.type = 'text/css'
+  link.rel = 'stylesheet'
+  console.log('color is ' + data.item)
+  document.getElementsByTagName('head')[0].appendChild(link)
 })
-let link = document.createElement('link')
-link.href = chrome.extension.getURL('css/' + item)
-link.type = 'text/css'
-link.rel = 'stylesheet'
-document.getElementsByTagName('head')[0].appendChild(link)
+
+/* chrome.storage.sync.get('item', function (data) {
+  console.log('color is ' + data.item)
+  this.window.insertCSS({file: 'css/' + data.item})
+}) */
 /* }
 window.onload = function () {
   script()
-}*/
+} */
