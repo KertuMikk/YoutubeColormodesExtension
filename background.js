7dac3fe66a05a051727142301d8607e191cfa49a
Kertu Mikk <31847340+KertuMikk@users.noreply.github.com>
2018-05-12 19:19:53 +0300

    Add files via upload

--- background.js
+++ background.js
@@ -11,17 +11,31 @@ chrome.runtime.onInstalled.addListener(function () {
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: { hostEquals: 'www.youtube.com', schemes: ['https'] }
+        pageUrl: { hostEquals: 'www.youtube.com', schemes: ['https'] }//, 
+        // css: ['css/' + item]
       })
       ],
       actions: [new chrome.declarativeContent.ShowPageAction()]
     }])
   })
 })
-/* chrome.runtime.onStartup.addListener(function () {
+
+chrome.tabs.onCreated.addListener(ThemeChange)
+chrome.tabs.onUpdated.addListener(function (tabId, info, tab) {
+  if (info.status == 'complete') ThemeChange(tab)
+})
+
+function ThemeChange (tab) {
+  let item
   chrome.storage.sync.get('item', function (data) {
     console.log('color is ' + data.item)
+    item = data.item
     chrome.tabs.insertCSS({file: 'css/' + data.item})
   })
-})
-*/
+  var tabUrl = 'https://www.youtube.com/*'
+  if (tabUrl && tabUrl.indexOf('https://www.youtube.com/*') != -1) {
+    chrome.tabs.insertCSS(tab.id, {
+      file: 'css/' + item
+    })
+  }
+}
