54e2709c766b03b80249ca692e41e255b7198104
Kertu Mikk <kertumik@tlu.ee>
2018-05-12 22:57:39 +0300

    options.html

--- options.js
+++ options.js
@@ -6,8 +6,8 @@ const kButtonTheme = ['rainbow', 'pastel', 'red', 'navy', 'B&W', 'Mint', 'rose']
 function constructOptions (kButtonTheme) {
   for (let item of kButtonTheme) {
     let button = document.createElement('button')
-    button.style.height = 40 + 'px'
-    button.style.width = 40 + 'px'
+    button.style.height = 150 + 'px'
+    button.style.width = 150 + 'px'
     button.style.backgroundImage = "url('https://raw.githubusercontent.com/KertuMikk/YoutubeColormodesExtension/master/images/" + item + "BIG.png')"
     let name = document.createElement('p') /* COMMENT OUT FROM LET NAME TO ELEMENT.APPEND.. TO MOVE COLOR SELECTION TO OPTIONS  */
     let node = document.createTextNode(item)
